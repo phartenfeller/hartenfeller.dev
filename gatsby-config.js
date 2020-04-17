1c382bef2a48e693f1e8286f8fcc6d4004e950ab
phartenfeller <pharti@hotmail.de>
2020-04-17 17:25:38 +0200

    changed website metadata #3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Philipp Hartenfeller`,
+    description: `Portfolio of software developer Philipp Hartenfeller`,
+    author: `Philipp Hartenfeller`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
