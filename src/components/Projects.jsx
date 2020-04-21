3c2f202a9c64f6eb10dd92275ecf369ddd795c26
phartenfeller <pharti@hotmail.de>
2020-04-21 10:13:17 +0200

    fix gh url cyf

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ const projects = {
     screenshot: 'cyftime-screenshot.png',
     description:
       'Website for a friend who makes rap music. Check his music and the site out.',
-    github: 'https://github.com/phartenfeller/cyftime_website',
+    githubUrl: 'https://github.com/phartenfeller/cyftime_website',
     projectUrl: 'https://cyftime.de',
     buttonType: 'green'
   }
