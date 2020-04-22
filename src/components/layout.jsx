bd9dd11205c4b2b31c50949432aa71317f57671c
phartenfeller <pharti@hotmail.de>
2020-04-22 21:47:00 +0200

    improved footer

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,6 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
+import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import './layout.css';
@@ -15,15 +16,29 @@ const Layout = ({ children }) => {
       <div className="min-h-screen flex flex-col">
         <main className="flex-grow">{children}</main>
         <footer
-          className="py-4 px-8 text-white text-sm md:text-md md:font-semibold shadow-lg grid grid-cols-2 md:grid-cols-3"
+          className="py-4 px-8 text-white text-sm md:text-md md:font-semibold shadow-lg grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5"
           style={{ background: '#544242' }}
         >
-          <div className="">{`© ${new Date().getFullYear()} Philipp Hartenfeller`}</div>
-          <div className="md:text-center">
+          <div className="my-4 lg:my-0">{`© ${new Date().getFullYear()} Philipp Hartenfeller`}</div>
+          <div className="my-4 lg:my-0 lg:text-center">
+            <Link className="py-2 pr-2 underline" to="/imprint">
+              Imprint
+            </Link>
+          </div>
+
+          <div className="my-4 lg:my-0 lg:text-center">
             This website does not use cookies
           </div>
-          <div className="underline md:text-right mt-4 md:mt-0">
-            <a href="https://github.com/phartenfeller/hartenfeller.dev">
+          <div className="my-4 lg:my-0 lg:text-center">
+            <Link className="py-2 pr-2 underline" to="/privacy">
+              Privacy Policy
+            </Link>
+          </div>
+          <div className="my-4 lg:my-0 lg:text-right">
+            <a
+              className="underline"
+              href="https://github.com/phartenfeller/hartenfeller.dev"
+            >
               Check out the code on GitHub
             </a>
           </div>
