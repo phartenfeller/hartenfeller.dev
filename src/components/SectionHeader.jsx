5f981c80b29bed26a6a704b06d659807e807278b
phartenfeller <pharti@hotmail.de>
2020-04-22 20:23:44 +0200

    fix double h2 s

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 const SectionHeader = ({ section }) => {
   return (
     <div className="mb-8 md:mb-16">
-      <h2 className="text-center text-2xl lg:text-4xl text-gray-800 font-bold font-mono">
+      <div className="text-center text-2xl lg:text-4xl text-gray-800 font-bold font-mono">
         <span>/*</span>
         <span className="hidden md:inline-block mx-2">------</span>
         <h2 className="inline-block mx-2"> {section} </h2>
         <span className="hidden md:inline-block mx-2">------</span>
         <span>*/</span>
-      </h2>
+      </div>
     </div>
   );
 };
