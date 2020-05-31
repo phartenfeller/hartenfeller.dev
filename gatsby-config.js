e785693b40b11c661e60d49cdcad11033ac928a4
phartenfeller <pharti@hotmail.de>
2020-05-31 19:24:03 +0200

    sitemap gen

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,8 @@ module.exports = {
   siteMetadata: {
     title: `Philipp Hartenfeller`,
     description: `Portfolio of software developer Philipp Hartenfeller`,
-    author: `Philipp Hartenfeller`
+    author: `Philipp Hartenfeller`,
+    siteUrl: `https://hartenfeller.dev`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -43,6 +44,31 @@ module.exports = {
           identifier: process.env.STRAPI_USER,
           password: process.env.STRAPI_PASSWORD
         }
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        exclude: ['/imprint', '/privacy'],
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+        }`,
+        resolveSiteUrl: () => {
+          return 'https://hartenfeller.dev';
+        },
+        serialize: ({ allSitePage }) =>
+          allSitePage.nodes.map(node => {
+            return {
+              url: `https://hartenfeller.dev${node.path}`,
+              changefreq: `weekly`,
+              priority: node.path !== '/' ? 0.7 : 1
+            };
+          })
       }
     }
   ]
