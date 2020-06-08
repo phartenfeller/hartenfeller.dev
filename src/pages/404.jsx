d7b27e0c291486c368f930d5b21a21e37bd46867
phartenfeller <pharti@hotmail.de>
2020-06-08 12:50:49 +0200

    #11 added links to home and blog

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,4 @@
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import React from 'react';
 import Layout from '../components/layout';
@@ -26,6 +26,21 @@ const NotFoundPage = () => {
           <h2 className="text-xl text-gray-300">
             Lost in Space - You just hit a route that doesn&#39;t exist.
           </h2>
+          <div className="text-center my-4 text-xl text">
+            <Link
+              to="/"
+              className="text-orange-400 hover:text-orange-600 hover:underline"
+            >
+              Homepage
+            </Link>
+            <span className="mx-4 text-gray-400">•</span>
+            <Link
+              to="/blog/"
+              className="text-orange-400 hover:text-orange-600 hover:underline"
+            >
+              Blog
+            </Link>
+          </div>
           <div className="w-2/3 m-auto">
             <Img fluid={data.astronautImage.childImageSharp.fluid} />
           </div>
