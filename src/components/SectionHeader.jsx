d8ee883ae35e7622a9c35c2d635cd5699965efeb
phartenfeller <pharti@hotmail.de>
2021-01-21 13:20:55 +0100

    fixed linting errors

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -5,18 +5,18 @@ const SectionHeader = ({ section }) => {
   return (
     <div className="mb-8 md:mb-16">
       <div className="text-center text-2xl lg:text-4xl text-gray-800 font-bold font-mono">
-        <span>/*</span>
+        <span>/&#42;</span>
         <span className="hidden md:inline-block mx-2">------</span>
         <h2 className="inline-block mx-2"> {section} </h2>
         <span className="hidden md:inline-block mx-2">------</span>
-        <span>*/</span>
+        <span>&#42;/</span>
       </div>
     </div>
   );
 };
 
 SectionHeader.propTypes = {
-  section: PropTypes.string.isRequired
+  section: PropTypes.string.isRequired,
 };
 
 export default SectionHeader;
